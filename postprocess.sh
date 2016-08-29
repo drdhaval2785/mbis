88197bebd645f46f5e9826f4082f8d3d9eb2d640
drdhaval2785 <drdhaval2785@gmail.com>
2016-08-29 21:10:59 +0530

    commented postprocess.sh

--- postprocess.sh
+++ postprocess.sh
@@ -1,4 +1,6 @@
 dos2unix hottiedata/output/positive.txt
 dos2unix hottiedata/output/negative.txt
+echo "Shifting predicted positive images to Images/sorted/positives..."
 while IFS= read -r filename; do mv "$filename" Images/Sorted/positives; done < hottiedata/output/positive.txt
+echo "Shifting predicted negative images to Images/sorted/negatives..."
 while IFS= read -r filename; do mv "$filename" Images/Sorted/negatives; done < hottiedata/output/negative.txt
