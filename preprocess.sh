cf734da69cfd725d0d869fce2d1469f3e3577df5
drdhaval2785 <drdhaval2785@gmail.com>
2016-08-29 21:08:41 +0530

    preprocess.sh commented

--- preprocess.sh
+++ preprocess.sh
@@ -1,3 +1,4 @@
+echo "Prepare the folder structure."
 rm -rf hottiedata
 mkdir hottiedata
 mkdir hottiedata/input
@@ -12,8 +13,13 @@ mkdir Images/ToBeSorted
 mkdir Images/Sorted
 mkdir Images/Sorted/positives
 mkdir Images/Sorted/negatives
+echo "Resize the images in Images/ImagesDir directory and store in hottiedata/input/trues.csv..."
 python resize.py Images/ImagesDir trues.csv trueset.txt
+echo "Resize the images in Images/JunkDir directory and store in hottiedata/input/falses.csv..."
 python resize.py Images/JunkDir falses.csv falseset.txt
+echo "Resize the images in Images/TestDir/positives directory and store in hottiedata/input/testpositives.csv..."
 python resize.py Images/TestDir/positives testpositives.csv testplus.txt
+echo "Resize the images in Images/TestDir/negatives directory and store in hottiedata/input/testnegatives.csv..."
 python resize.py Images/TestDir/negatives testnegatives.csv testminus.txt
+echo "Resize the images in Images/ToBeSorted directory and store in hottiedata/input/tobesorted.csv..."
 python resize.py Images/ToBeSorted tobesorted.csv tosort.txt
